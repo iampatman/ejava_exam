f979039b87606ec2e467251ef2705990b61a7450
Nguyen Trung <nguyentrung0904@gmail.com>
2016-11-24 13:38:20 +0800

    update

--- src/java/business/DepartmentBean.java
+++ src/java/business/DepartmentBean.java
@@ -7,11 +7,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author NguyenTrung
